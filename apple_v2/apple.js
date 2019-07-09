147cf30942f0fc54220ec3be8056af3f400e98be
Alex Stetsyuk <39220935+loboSabio@users.noreply.github.com>
2019-07-09 23:49:49 +0300

    Add files via upload

---
+++ apple_v2/apple.js
@@ -0,0 +1,28 @@
+document.getElementById("search").addEventListener('click', function () {
+    let search = prompt("search product", "iPhone").toLowerCase();
+    if (search === 'apple') {
+        document.open("https://www.apple.com/ru/", '', '');
+    } else if (search === 'mac') {
+        document.open("https://www.apple.com/ru/mac/", '', '');
+    } else if (search === 'ipad') {
+        document.open("https://www.apple.com/ru/ipad/", '', '');
+    } else if (search === 'iphone') {
+        document.open("https://www.apple.com/ru/iphone/", '', '');
+    } else if (search === 'watch') {
+        document.open("https://www.apple.com/ru/watch/", '', '');
+    } else if (search === 'tv') {
+        document.open("https://www.apple.com/ru/tv/", '', '');
+    } else if (search === 'music') {
+        document.open("https://www.apple.com/ru/music/", '', '');
+    } else if (search === 'support') {
+        document.open("https://support.apple.com/ru-ru", '', '');
+    } else {
+        alert("Wrong command!");
+    }
+})
+document.getElementById("bag").addEventListener('click', function () {
+    document.open("https://www.apple.com/ru/shop/accessories/all-accessories/whats-new", '', '')
+})
+document.getElementById("buy").addEventListener('click', function () {
+    document.open("https://www.apple.com/ru/shop/buy-iphone/iphone-xr", '', '');
+})
